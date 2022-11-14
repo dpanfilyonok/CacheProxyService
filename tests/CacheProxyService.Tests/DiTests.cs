7413baa285f1b20a9f934db2558545f9eea25603
Dmitriy Panfilyonok <dmitriy.panfilyonok@gmail.com>
2022-11-14 11:05:11 +0300

    Add DI tests

---
+++ tests/CacheProxyService.Tests/DiTests.cs
@@ -0,0 +1,21 @@
+﻿using CacheProxyService.Repositories;
+using CacheProxyService.Services;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace CacheProxyService.Tests;
+
+public class DiTests
+{
+    [Fact]
+    public void AllDependenciesAreRegistered()
+    {
+        var services = new WebApplicationFactory<Program>().Services;
+
+        services.GetRequiredService<ILocationService>();
+        services.GetRequiredService<LocationsRepository>();
+        services.GetRequiredService<CachedLocationsRepository>();
+        services.GetRequiredService<LocationsRepositoryResolver>();
+    }
+}
